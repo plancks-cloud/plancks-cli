61e92586d0ad4af49fa2edfd791419c9db82d177
Just1689 <Just1689@gmail.com>
2019-02-27 22:59:14 +0200

    Install time and go sum

--- model/repository.go
+++ model/repository.go
@@ -7,7 +7,7 @@ const AppPort = 6227
 const ListentPort = 6227
 
 //InstallMaxHealthChecks is the number of times it will check for the health service during install
-const InstallMaxHealthChecks = 5
+const InstallMaxHealthChecks = 24
 
 //InstallSleepBetweenChecksis is the number of seconds the app will wait between checking for the health service during install
 const InstallSleepBetweenChecks = 5
